2c8c7fc9f2e59e0ee920113cb2a88acb00e4fd24
Ekkaira <denysxchebotar@gmail.com>
2024-02-18 14:57:52 +0200

    removed version generation

--- config/gulp-tasks/html.js
+++ config/gulp-tasks/html.js
@@ -1,5 +1,4 @@
-import versionNumber from "gulp-version-number";
-import webpHtmlNosvg from "gulp-webp-html-nosvg";
+import webpHtmlNosvg from "gulp-webp-html-nosvg"
 
 export const html = () => {
 	return app.gulp.src(`${app.path.build.html}*.html`)
@@ -15,16 +14,16 @@ export const html = () => {
 				webpHtmlNosvg()
 			)
 		)
-		.pipe(versionNumber({
-			'value': '%DT%',
-			'append': {
-				'key': '_v',
-				'cover': 0,
-				'to': ['css', 'js', 'img']
-			},
-			'output': {
-				'file': 'config/version.json'
-			}
-		}))
+		// .pipe(versionNumber({
+		// 	'value': '%DT%',
+		// 	'append': {
+		// 		'key': '_v',
+		// 		'cover': 0,
+		// 		'to': ['css', 'js', 'img']
+		// 	},
+		// 	'output': {
+		// 		'file': 'config/version.json'
+		// 	}
+		// }))
 		.pipe(app.gulp.dest(app.path.build.html));
 }
