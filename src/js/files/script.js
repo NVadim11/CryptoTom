3842f6d6c32b6916b20e42b2c920d3ea76ea047b
NVadim <palaginvad@gmail.com>
2024-02-19 13:30:48 +0200

    audio test

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,12 +3,11 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
-const soundOn = document.querySelector(".soundToggler__itemOn");
-const soundOff = document.querySelector(".soundToggler__itemOff");
+const play = document.querySelector(".soundToggler__itemOn");
+const pause = document.querySelector(".soundToggler__itemOff");
 
-// if (soundOn) {
-//     loginBtn.addEventListener("click", function(e){
-//     e.stopPropagation();
-//     authMenu.classList.toggle('flex');
-// });
-    
+function toggleMute() {
+    var myAudio = document.getElementById('audio');
+    myAudio.muted = !myAudio.muted;
+ }
+
